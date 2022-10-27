18cd03d4ba51cd92d40761fce3c334082dcd8abc
abelmuro <abelmuro93@gmail.com>
2022-10-27 13:36:54 -0700

    commit

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {auth} from '../firebase-config';
 import {useNavigate} from 'react-router-dom';
 import styles from './styles.module.css';
+import googleIcon from './images/google icon.png';
+import appleIcon from './images/apple icon.png';
 
 function SignIn() {
     const navigate = useNavigate();
@@ -10,15 +12,21 @@ function SignIn() {
         <section className={styles.background}>
             <div className={styles.signInBox}>
                 <h1 className={styles.title}>
-                    Welcome to Insta Chatter Box!
+                    Welcome to the Insta Chatter Box!
                 </h1>
                 <p className={styles.desc}>
-                    This app will enable you to send messages 
+                    This app will enable you to send messages,
+                    but you must first sign in with your google 
+                    or apple account 
                 </p>
-                <div className={styles.inputContainer}>
-                    <input type="text" className={styles.input} />                    
-                </div>
-
+                <button className={styles.signInButton}>
+                    <img src={googleIcon} className={styles.googleIcon}/>
+                    <p className={styles.desc}>Sign in with Google</p>
+                </button>   
+                <button className={styles.signInButton}>
+                    <img src={appleIcon} className={styles.appleIcon}/>
+                    <p className={styles.desc}>Sign in with Apple</p>
+                </button>                
             </div>
         </section>
     )
