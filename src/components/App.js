8f0f4389a7adc169fc4e5e962ea218bbaa1120c2
abelmuro <abelmuro93@gmail.com>
2022-10-26 21:33:16 -0700

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,59 @@
 import React from 'react';
 import {db} from './firebase-config';
-import {collection, addDoc, setDoc, doc} from 'firebase/firestore'
+import {collection, addDoc ,orderBy, limit, query} from 'firebase/firestore';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+import {v4 as uuid} from 'uuid';
+import './styles.css';
 
 
 function App(){
-    //const collectionRef = collection(db, "users");            //selects a collection
-    //const docRef = (collectionRef, "Abel")                    //selects a document within the collection
+    const collectionRef = collection(db, "messages");
+    //TODO: find a property that you can use to order all the documents in the collection
+    const q = query(collectionRef, orderBy("message"), limit(25)); 
 
+    const [messages, loading, error] = useCollectionData(q);
 
-    async function addDocument () {
+    const sendMessage = async () => {
         try{
-            const newDocument = collection(db, "example")
-            await addDoc(newDocument, {
-                first: "abel",
-                last: "muro",
-                born: "july 56"
-            });
+            console.log()
+            const currentDate = new Date();
+            const readableDate = currentDate.toDateString();
+            const hour = currentDate.getHours();
+            const minutes = currentDate.getMinutes();
+            const seconds = currentDate.getSeconds();
+            const messageEntered = readableDate + ", Hour:" + hour + " Minutes:" + minutes + " Seconds:" +  seconds;
+            let input = document.querySelector(".input");
+            const docRef = await addDoc(collectionRef, {message: input.value, timeStamp: messageEntered})
         }
         catch(error){
-            console.log("error");
+            console.log(error)
         }
-    };
+    }
 
-    (async function replaceDocument() {
-        try{
-            const newDocument = doc(db, "cities", "LA");
-            await setDoc(newDocument, {
-                name: "abelito"
-            })
-        }
-        catch(error){
-            console.log(error);
-        }
-    })();
+    const handleClick = (e) => {
+        e.target.value = "";
+    }
 
+    return loading ? (<>...is loading</>) : (
+        <main>
+            <div className={"chatBox"}>
+            {messages.map((message) => {
+                return (
+                    <div key={uuid()}>
+                        <p className={"chatBubbles"}>{message.message}</p>
+                        <br/>
+                    </div>
+                )
+            })}                
+            </div>
 
-    //TODO: read data from firestore
+            <div className={"inputContainer"}>
+                <input type="text" className={"input"} defaultValue={"Enter message here..."} onClick={handleClick}/>
+                <button onClick={sendMessage} className={"sendMessage"}>Send Message</button>                
+            </div>
 
-
-    return(<>hello world</>)
+        </main>
+        )
 }
 
 export default App;
